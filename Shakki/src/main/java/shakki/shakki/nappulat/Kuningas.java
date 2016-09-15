5f6d1d55520673d5319604b015d22af8f7531aab
Sami Hirvonen <Sami Hirvonen>
2016-09-15 18:34:56 +0300

    projektin hakemisto

---
+++ Shakki/src/main/java/shakki/shakki/nappulat/Kuningas.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shakki.shakki.nappulat;
+import shakki.shakki.Nappula;
+import shakki.shakki.Pelaaja;
+/**
+ *
+ * @author Sami
+ */
+public class Kuningas implements Nappula {
+    
+    private Pelaaja pelaaja;
+    
+    public Kuningas(Pelaaja pelaaja) {
+        
+        this.pelaaja = pelaaja;
+        
+    }
+    
+    public Pelaaja getPelaaja() {
+        
+        return pelaaja;
+        
+    }
+    
+    public int getSiirrot() {
+        
+        return 10;
+    }
+    
+    public void kasvataSiirtojenLkm(){
+        
+    }
+    
+}
